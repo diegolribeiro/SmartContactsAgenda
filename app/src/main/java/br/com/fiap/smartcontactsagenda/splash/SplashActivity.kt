391e0b3269391a73ece8c900fe9960f547bb8a06
Diego Landi Ribeiro <33814620+diegolribeiro@users.noreply.github.com>
2019-09-14 19:49:22 -0300

    Criando a Tela de Splash

---
+++ app/src/main/java/br/com/fiap/smartcontactsagenda/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.smartcontactsagenda.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.smartcontactsagenda.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
